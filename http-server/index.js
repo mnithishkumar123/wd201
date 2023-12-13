6488240d914ead5d9d549665300ad630a8837e63
mnithishkumar <mnithishkumar123@example.comgit config --global user.name mnithishkumar>
2023-12-14 00:46:42 +0530

    initial commit

---
+++ http-server/index.js
@@ -0,0 +1,67 @@
+// const http = require("http");
+// const fs = require("fs");
+// fs.readFile("home.html", (err, home) => {
+//     if (err) {
+//         throw err;
+//     }
+//     http
+//         .createServer((request, response) => {
+//             response.writeHeader(200, { "Content-Type": "text/html" });
+//             response.write(home);
+//             response.end();
+//         })
+//         .listen(3000);
+// });
+const http = require("http");
+const fs = require("fs");
+const port = require("minimist")(process.argv.slice(2));
+
+let homeContent = "";
+let projectContent = "";
+let registrationcontent = "";
+
+fs.readFile("home.html", (err, home) => {
+    if (err) {
+        throw err;
+    }
+    homeContent = home;
+});
+
+fs.readFile("home.html", (err, home) => {
+    if (err) {
+        throw err;
+    }
+    homeContent = home;
+});
+fs.readFile("project.html", (err, project) => {
+    if (err) {
+        throw err;
+    }
+    projectContent = home;
+});
+fs.readFile("registration.html", (err, home) => {
+    if (err) {
+        throw err;
+    }
+    registrationcontent = home;
+});
+http
+    .createServer((request, response) => {
+        let url = request.url;
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        switch (url) {
+            case "/project":
+                response.write(projectContent);
+                response.end();
+                break;
+            case "/registration":
+                response.write(registrationcontent);
+                response.end();
+                break;
+            default:
+                response.write(homeContent);
+                response.end();
+                break;
+        }
+    })
+    .listen(port);
